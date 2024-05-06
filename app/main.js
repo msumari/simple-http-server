f13d0287eff7faa05bb1b0c44870d6706683f336
msumari <suleykhery@gmail.com>
2024-05-06 02:48:11 +0100

    Respond with 400

--- app/main.js
+++ app/main.js
@@ -3,13 +3,29 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 // console.log("Logs from your program will appear here!");
 
-
 // Uncomment this to pass the first stage
-const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n\n");
-  socket.on("close", () => {
-    socket.end();
-    server.close();
+const requestParser = (data) => {
+  const dataString = data.toString("utf-8");
+  const [firstLine, host] = dataString.split("\r\n");
+  const [method, path, protocol] = firstLine.split(" ");
+  return { method, path, protocol };
+};
+
+const server = net.createServer();
+server.on("connection", (socket) => {
+  console.log("Client connected");
+
+  socket.on("data", (data) => {
+    const { method, path, protocol } = requestParser(data);
+    if (path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n\n");
+      socket.end();
+      server.close();
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n\n");
+      socket.end();
+      server.close();
+    }
   });
 });
 
